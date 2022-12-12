681f31bfe2829d385b73837c5e12a3e8edf216e7
darkimpow <darkimpowell@gmail.com>
2022-12-12 14:33:33 -0500

    updated functions for div box

--- app.js
+++ app.js
@@ -1,49 +1,15 @@
-var myinput= document.getElementById("psw");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
+//validate upper case letters
+function checkUC() {
 
- 
+    let uppercasepattern=/[A-Z]/g;
 
-// validate lowercase letters 
-var lowerCaseLetters=/[a-z]/g;
 
-if(myinput.value.match(lowerCaseLetters)){
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-} else{
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-}
+    // recive password from the input feild
+    let pswvalue = document.getElementById(psw).value;
 
-// validate Uppercase letters 
-var UpperCaseLetters=/[A-Z]/g;
-
-if(myinput.value.match(UpperCaseLetters)){
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-} else{
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-}
-
-//validate Numbers
-var numbers=/[0-9]/g;
-
-if(myinput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-} else{
-    number.classList.remove("valid");
-    number.classList.remove("invalid");
-}
-
-//validate Length
-if(myinput.value.length >=8) {
-    length.classList.remove("invalid");
-    length.classList.add("invalid");
-} else{
-    length.classList.remove("valid");
-    length.classList.add("invalid")
+    ////verify password has an uppercase letter
+    let answerUC = uppercasepattern.test(pswvalue)
+    
+    //display if password has a uppercase letter    
+    document.getElementById('capital').innerHTML = answerUC;
 }
