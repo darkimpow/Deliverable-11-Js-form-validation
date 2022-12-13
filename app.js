c48f451e44afd467e674797078625fa2916502b5
darkimpow <darkimpowell@gmail.com>
2022-12-13 11:04:52 -0500

    updated js file functions with color coded true or false options for the given password entered & adjusted lines 20-32 in html file

--- app.js
+++ app.js
@@ -1,15 +1,62 @@
-//validate upper case letters
 function checkUC() {
-
     let uppercasepattern=/[A-Z]/g;
-
-
-    // recive password from the input feild
-    let pswvalue = document.getElementById(psw).value;
-
-    ////verify password has an uppercase letter
-    let answerUC = uppercasepattern.test(pswvalue)
+    let pswvalue = document.getElementById('psw').value ;
+    let answerUC = uppercasepattern.test(pswvalue) ;
+// your password contains a uppercase letter 
+    if(answerUC=== true) {
+        document.getElementById('capital').innerHTML="Your password has a lowercase letter";
+        document.getElementById ('capital').style.fontWeight = "bold"
+        document.getElementById('capital').className="bg-green-500"
+    }
+    //if password is not containing a Uppercase letter 
+    else{
+        document.getElementById('capital').innerHTML="you are missing a Uppercase letter";
+        document.getElementById ('capital').style.fontWeight = "bold"
+        document.getElementById('capital').className="bg-red-500"
+    }
+}
+function checkLC() {
+    let lowercasepattern=/[a-z]/g;
+    let pswvalue = document.getElementById('psw').value ;
+    let answerLC = lowercasepattern.test(pswvalue) ;
     
-    //display if password has a uppercase letter    
-    document.getElementById('capital').innerHTML = answerUC;
+    // if password contains a lowercase letter
+if(answerLC=== true) {
+        document.getElementById('messageLC').innerHTML="Your password has a lowercase letter";
+        document.getElementById ('messageLC').style.fontWeight = "bold"
+        document.getElementById('messageLC').className="bg-green-500"
+    }
+    //if password is not containing a lowercase letter 
+    else{
+        document.getElementById('messageLC').innerHTML="you are missing a lowercase letter";
+        document.getElementById ('messageLC').style.fontWeight = "bold"
+        document.getElementById('messageLC').className="bg-red-500"
+    }
+}
+function checkNB() {
+    let numberpattern=/[0-9]/g;
+    let pswvalue = document.getElementById('psw').value ;
+    let answerNB = numberpattern.test(pswvalue) ;
+
+    // if password contains a number
+if(answerNB=== true) {
+        document.getElementById('messageNB').innerHTML="Your password has a number";
+        document.getElementById ('messageNB').style.fontWeight = "bold"
+        document.getElementById('messageNB').className="bg-green-500"
+    }
+    //if password does not contain a number
+    else{
+        document.getElementById('messageNB').innerHTML="you are missing a number";
+        document.getElementById ('messageNB').style.fontWeight = "bold"
+        document.getElementById('messageNB').className="bg-red-500"
+    }
+}
+function checkLen() {
+    let pswvalue = document.getElementById('psw').value;
+    if (pswvalue.length >= 8) {
+    document.getElementById('messagelen').innerHTML="Good job your password is long enough" ;
+    }
+    else{
+        document.getElementById('messagelen').innerHTML="Warning your password needs to be longer";
+    }
 }
